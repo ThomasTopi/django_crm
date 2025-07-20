4d2c640f08bb0fb44bdaeb874dbc512863353ea1
Tomas Svoboda <fredka171@gmail.com>
2025-07-20 17:25:38 +0200

    Add initial application structure and user authentication features
    - Created a new runserver.bat file for easy server startup.
    - Updated ASGI settings for consistency.
    - Modified settings to include 'myapp' and set template directories.
    - Enhanced URL routing to include 'myapp' URLs.
    - Implemented user authentication views and templates for login/logout.
    - Added home and user-specific home templates.
    - Introduced custom CSS for styling and layout.
    - Included a main photo for visual enhancement.
    - Updated database file to reflect changes.

--- crm/myapp/urls.py
+++ crm/myapp/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.home, name="home")]
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+]
--- crm/myapp/views.py
+++ crm/myapp/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 
-# Create your views here.
+
+def home(request):
+    if request.user.is_authenticated:
+        return render(request, "home_user.html", {})
+    else:
+        return render(request, "home.html", {})
+
+
+def login_view(request):
+    error = None
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return redirect("home")
+        else:
+            error = "Nesprávné uživatelské jméno nebo heslo."
+    return render(request, "login.html", {"error": error})
+
+
+def logout_view(request):
+    auth_logout(request)
+    return redirect("home")
